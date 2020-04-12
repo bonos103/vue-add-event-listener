9e59c5784b42a509b659a0a0a171e79640a5da8a
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2020-04-12 23:15:01 +0900

    change bundler to rollupjs

---
+++ build/rollup.config.js
@@ -0,0 +1,30 @@
+import buble from 'rollup-plugin-buble' // 適切にブラウザをサポートするトランスパイラおよびポリフィル
+
+const globals = {
+  vue: 'Vue',
+}
+
+export default {
+    input: 'index.js', // Path relative to package.json
+    output: [
+      {
+        file: 'dist/index.umd.js',
+        format: 'umd',
+        globals,
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'es',
+        globals,
+      },
+      {
+        file: 'dist/index.min.js',
+        format: 'iife',
+        globals,
+      },
+    ],
+    external: ['vue'],
+    plugins: [
+        buble(), // ES5 へトランスパイルする
+    ],
+}
